3535d89ef4f96fd3f82f297297aec9204a0e2f09
Subham kumar <subham2811singh@gmail.com>
2026-06-14 12:45:57 +0530

    Time: 51 ms (43.78%), Space: 29 MB (72.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-       vector<vector<int>> ans;
-       sort(nums.begin(),nums.end());
-       for(int i = 0;i<nums.size();i++){
-            int tar = -nums[i];
-            int low = i+1;
-            int high = nums.size()-1;
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-            while(low<high){
-                int sum = nums[low] + nums[high];
-                if(sum == tar){
-                    vector<int> arr;
-                    arr.push_back(nums[i]);
-                    arr.push_back(nums[low]);
-                    arr.push_back(nums[high]);
-                    ans.push_back(arr);
-                    while(low < high && nums[low] == nums[low+1]) low++;
-                    while(high > 0 && nums[high] == nums[high-1]) high--;
-                    low++;
-                    high--;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<nums.size();i++){
+            if(i > 0 && nums[i-1] == nums[i]){
+                continue;
+            }
+            int st = i+1;
+            int en = nums.size()-1;
+            while(st < en){
+                int sum = nums[i] + nums[st] + nums[en];
+                if(sum==0){
+                    ans.push_back({nums[i],nums[st],nums[en]});
+                    while(st < en && nums[st] == nums[st+1]){
+                        st++;
+                    }
+                    while(en > 0 && nums[en] == nums[en-1]){
+                        en--;
+                    }
+                    st++;
+                    en--;
                 }
-                else if(sum < tar){
-                    low++;
+                else if(sum > 0){
+                    en--;
                 }
                 else{
-                    high--;
+                    st++;
                 }
             }
-       }
-       return ans;
+        }
+        return ans;
     }
 };
