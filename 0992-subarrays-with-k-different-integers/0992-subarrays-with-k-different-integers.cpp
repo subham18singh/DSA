7fa7d9ceec15c6614090105f9ee4ff1e343852ec
Subham kumar <subham2811singh@gmail.com>
2026-06-24 10:05:00 +0530

    Time: 87 ms (44.67%), Space: 56.1 MB (56.1%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-
-        unordered_map<int, int> mp;
-
+    int count(vector<int>& nums, int k){
         int st = 0;
-        int prefix = 0;
+        unordered_map<int,int> mp;
         int ans = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-
+        for(int i = 0;i<nums.size();i++){
             mp[nums[i]]++;
-
-            if (mp.size() > k) {
-
-                while (mp.size() > k) {
-
-                    mp[nums[st]]--;
-
-                    if (mp[nums[st]] == 0)
-                        mp.erase(nums[st]);
-
-                    st++;
-                }
-
-                prefix = 0;
-            }
-
-            while (mp[nums[st]] > 1) {
-
+            while(mp.size() > k){
                 mp[nums[st]]--;
-
+                if(mp[nums[st]] == 0){
+                    mp.erase(nums[st]);
+                }
                 st++;
-                prefix++;
             }
-
-            if (mp.size() == k)
-                ans += prefix + 1;
+            ans+=(i-st+1);
         }
-
         return ans;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return count(nums,k) - count(nums,k-1);
     }
 };
