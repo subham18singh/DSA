99f743fc09b5e3d19320d33f1b162b46f9fa0093
Subham kumar <subham2811singh@gmail.com>
2026-07-01 00:37:56 +0530

    Time: 54 ms (71.08%), Space: 23.9 MB (56.65%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,16 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (!headA || !headB) return nullptr;
-
-        ListNode* a = headA;
-        ListNode* b = headB;
-
-        while (a != b) {
-            a = (a == nullptr) ? headB : a->next;
-            b = (b == nullptr) ? headA : b->next;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while(tempA!=NULL){
+            cnt1++;
+            tempA = tempA->next;
         }
-
-        return a; // either intersection node or nullptr
+        while(tempB!=NULL){
+            cnt2++;
+            tempB = tempB->next;
+        }
+        int dif = max(cnt1,cnt2) - min(cnt1,cnt2);
+        ListNode* skipb = headB;
+        ListNode* skip = headA;
+        if(cnt1 > cnt2){
+            while(dif!=0){
+                skip = skip->next;
+                dif--;
+            }
+        }
+        else{
+            while(dif!=0){
+                skipb = skipb->next;
+                dif--;
+            }
+        }
+        while(skip!=NULL && skipb!=NULL){
+            if(skip == skipb){
+                return skip;
+            }
+            skip = skip->next;
+            skipb = skipb->next;
+        }
+        return NULL;
     }
 };
