ab9c9bdf6c356305d67f131dbc42eaccdffc1793
Subham kumar <subham2811singh@gmail.com>
2026-07-01 00:47:35 +0530

    Time: 57 ms (55.17%), Space: 24 MB (26.1%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,40 +9,59 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        ListNode* tempA = headA;
-        ListNode* tempB = headB;
-        while(tempA!=NULL){
-            cnt1++;
-            tempA = tempA->next;
-        }
-        while(tempB!=NULL){
-            cnt2++;
-            tempB = tempB->next;
-        }
-        int dif = abs(cnt1-cnt2);
-        ListNode* skipb = headB;
-        ListNode* skip = headA;
-        if(cnt1 > cnt2){
-            while(dif!=0){
-                skip = skip->next;
-                dif--;
+        // int cnt1 = 0;
+        // int cnt2 = 0;
+        // ListNode* tempA = headA;
+        // ListNode* tempB = headB;
+        // while(tempA!=NULL){
+        //     cnt1++;
+        //     tempA = tempA->next;
+        // }
+        // while(tempB!=NULL){
+        //     cnt2++;
+        //     tempB = tempB->next;
+        // }
+        // int dif = abs(cnt1-cnt2)
+        // ListNode* skipb = headB;
+        // ListNode* skip = headA;
+        // if(cnt1 > cnt2){
+        //     while(dif!=0){
+        //         skip = skip->next;
+        //         dif--;
+        //     }
+        // }
+        // else{
+        //     while(dif!=0){
+        //         skipb = skipb->next;
+        //         dif--;
+        //     }
+        // }
+        // while(skip!=NULL && skipb!=NULL){
+        //     if(skip == skipb){
+        //         return skip;
+        //     }
+        //     skip = skip->next;
+        //     skipb = skipb->next;
+        // }
+        // return NULL;
+
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while(a!=b){
+            if(a == NULL){
+                a = headB;
+            }
+            else{
+                a = a->next;
+            }
+            if(b == NULL){
+                b = headA;
+            }
+            else{
+                b = b->next;
             }
         }
-        else{
-            while(dif!=0){
-                skipb = skipb->next;
-                dif--;
-            }
-        }
-        while(skip!=NULL && skipb!=NULL){
-            if(skip == skipb){
-                return skip;
-            }
-            skip = skip->next;
-            skipb = skipb->next;
-        }
-        return NULL;
+        return a;
     }
 };
