0546046ef6c889d2e7f81be16caae096bf023b4f
Subham kumar <subham2811singh@gmail.com>
2026-07-17 01:51:17 +0530

    Time: 8 ms (7.02%), Space: 9.9 MB (25.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+        map<char,int> mp;
+        for(int i = 0;i<s.length();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        for(auto it : mp){
+            if(it.second  != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
