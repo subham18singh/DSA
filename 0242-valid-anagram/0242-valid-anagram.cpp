29c4931aec6c01ea6693d3c3d78a1a249a153bad
Subham kumar <subham2811singh@gmail.com>
2026-07-17 01:55:13 +0530

    Time: 0 ms (100%), Space: 9.8 MB (25.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,7 +4,7 @@ public:
         if(s.length() != t.length()){
             return false;
         }
-        vector<int> mp(26,0);
+        int mp[26] = {0};
         for(int i = 0;i<s.length();i++){
             mp[s[i] - 'a']++;
             mp[t[i] - 'a']--;
