f5997956e7f60217657c4be01cc25f07734de15b
Subham kumar <subham2811singh@gmail.com>
2026-07-17 01:40:44 +0530

    Time: 3 ms (6.03%), Space: 8.1 MB (47.42%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -5,23 +5,28 @@ public:
             return false;
         }
         string temp = s+s;
-        int d = goal.length();
-        for(int i = 0;i<goal.length();i++){
-            bool flag = true;
-            string st = temp.substr(i,d);
-            for(int j = 0;j<goal.length();j++){
-                if(st[j] == goal[j]){
-                    continue;
-                }
-                else{
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                return true;
-            }
-        } 
-        return false;      
+
+        // correct but not optimal jsut to clear the logic;
+
+        // int d = goal.length();
+        // for(int i = 0;i<goal.length();i++){
+        //     bool flag = true;
+        //     string st = temp.substr(i,d);
+        //     for(int j = 0;j<goal.length();j++){
+        //         if(st[j] == goal[j]){
+        //             continue;
+        //         }
+        //         else{
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag){
+        //         return true;
+        //     }
+        // } 
+        // return false;      
+
+        return temp.find(goal) != string::npos;
     }
 };
