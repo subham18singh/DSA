4535803ccea8a0033a158d781b8bde7816791c16
Subham kumar <subham2811singh@gmail.com>
2026-07-26 00:41:21 +0530

    Time: 3 ms (54.57%), Space: 10.8 MB (80.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         for(int i = 0;i<s.length();i++){
             mp[s[i]]++;
         }
