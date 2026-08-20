6069e11350471bf92129697cd74805484aa62e7c
Subham kumar <subham2811singh@gmail.com>
2026-08-20 13:06:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (89.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,57 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
-        string open = "";
-        int last = 0;
-        for(int i = 0;i<s.length();i++){
-            if(s[i] == '('){
-                cnt1++;
-                last++;
-                open+=s[i];
+        stack<char> st;
+        for(char x:s){
+            if(x == '(' || x == '{' || x == '['){
+                st.push(x);
             }
-            else if(s[i] == '{'){
-                cnt2++;
-                last++;
-                open+=s[i];
-            }
-            else if(s[i] == '['){
-                cnt3++;
-                last++;
-                open+=s[i];
-            }
-            else if(s[i] == ')'){
-                if(cnt1 == 0 || open[last-1] != '('){
+            else{
+                if(st.empty()){
                     return false;
                 }
-                cnt1--;
-                open.pop_back();
-                last--;
-            }
-            else if(s[i] == '}'){
-                if(cnt2 == 0 || open[last-1] != '{'){
+                if(x == ')' && st.top() != '('){
                     return false;
                 }
-                cnt2--;
-                open.pop_back();
-                last--;
-            }
-            else if(s[i] == ']'){
-                if(cnt3 == 0 || open[last-1] != '['){
+                if(x == '}' && st.top() != '{'){
                     return false;
                 }
-                cnt3--;
-                open.pop_back();
-                last--;
+                if(x == ']' && st.top() != '['){
+                    return false;
+                }
+                st.pop();
             }
         }
-        if(cnt1 == 0 && cnt2 == 0 && cnt3 == 0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return st.empty();
     }
 };
